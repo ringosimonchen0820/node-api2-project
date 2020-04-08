627ac0dd19e46b6bcd29c334a2d392fc81abcdcf
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-04-08 17:40:32 -0400

    creater server and router, create get request handler for /api/posts

---
+++ data/db.router.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+
+const Data = require('./db');
+
+const router = express.Router();
+
+//* get <<<<< posts data
+router.get('/', (req, res) => {
+    Data.find()
+    .then(data => {
+        res.status(200).json({ success: true, data });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ success: false, Message: 'posts information can not be retrieved', err });
+    })
+});
